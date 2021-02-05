b0be5e9cb90e428500fb7539bc81e73b035ac5c9
sbboard <colin.buffum@gmail.com>
2021-02-05 13:44:13 -0700

    images saved to array

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 var imgNum = 0;
-var uploadedImages;
+var uploadedImages = [];
 var timeCalc = 0;
 var fileTag = document.getElementById("filetag");
 var minPerImg = document.getElementById("minPerImg");
+/////////////
+//////START UP
+////////////
 function changeMinPerImg(input) {
     if (input.value.length > 0) {
         timeCalc = imgNum * parseInt(input.value);
@@ -14,18 +17,22 @@ function changeMinPerImg(input) {
 }
 function changeImage(input) {
     if (input.files) {
+        //clear images
         document.getElementById("thumbnails").innerHTML = "";
+        uploadedImages = [];
         imgNum = input.files.length;
         //go through images
         for (var i = 0; i < input.files.length; i++) {
             var reader = new FileReader();
             reader.onload = function (e) {
+                uploadedImages.push(e.target.result);
                 var newImg = document.createElement("img");
                 newImg.src = e.target.result;
                 document.getElementById("thumbnails").appendChild(newImg);
             };
             reader.readAsDataURL(input.files[i]);
         }
+        console.log(uploadedImages);
         document.getElementById("queueNum").innerHTML = imgNum.toString();
         changeMinPerImg(minPerImg);
     }
@@ -43,3 +50,6 @@ function startApp() {
         }
     }
 }
+////////////////////
+////RUNNING
+///////////////////
--- app.ts
+++ app.ts
@@ -1,9 +1,12 @@
 let imgNum: number = 0;
-let uploadedImages: any[];
+let uploadedImages: string[] = [];
 let timeCalc: number = 0;
 let fileTag = <HTMLInputElement>document.getElementById("filetag");
 let minPerImg = <HTMLInputElement>document.getElementById("minPerImg");
 
+/////////////
+//////START UP
+////////////
 function changeMinPerImg(input: HTMLInputElement) {
   if (input.value.length > 0) {
     timeCalc = imgNum * parseInt(input.value);
@@ -15,18 +18,23 @@ function changeMinPerImg(input: HTMLInputElement) {
 
 function changeImage(input: HTMLInputElement) {
   if (input.files) {
+    //clear images
     document.getElementById("thumbnails").innerHTML = "";
+    uploadedImages = [];
+
     imgNum = input.files.length;
     //go through images
     for (let i = 0; i < input.files.length; i++) {
       let reader = new FileReader();
       reader.onload = function (e) {
+        uploadedImages.push(e.target.result as string);
         var newImg = document.createElement("img") as HTMLImageElement;
         newImg.src = e.target.result as string;
         document.getElementById("thumbnails").appendChild(newImg);
       };
       reader.readAsDataURL(input.files[i]);
     }
+    console.log(uploadedImages);
     document.getElementById("queueNum").innerHTML = imgNum.toString();
     changeMinPerImg(minPerImg);
   }
@@ -43,3 +51,7 @@ function startApp() {
     }
   }
 }
+
+////////////////////
+////RUNNING
+///////////////////
